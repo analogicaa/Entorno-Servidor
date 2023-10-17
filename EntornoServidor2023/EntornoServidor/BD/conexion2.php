db7f4997cc9a2cd2778b235fc07e5421daada885
CEP-AULA7-PC13\TECH <aherrera0136@ceuandalucia.es>
2023-10-17 17:38:37 +0200

    Actualizo archivos de BD y Formulario Inmobiliaria

--- EntornoServidor2023/EntornoServidor/BD/conexion2.php
+++ EntornoServidor2023/EntornoServidor/BD/conexion2.php
@@ -4,19 +4,67 @@ $username = "root";     // Nombre de usuario de la base de datos
 $password = "";            // Contraseña del usuario
 $database = "FP"; // Nombre de la base de datos
 
-// Crear una conexión
+// CREAR CONEXIÓN
 $conn = new mysqli($servername, $username, $password, $database);
 
-// Verificar la conexión
+// VERIFICAR CONEXIÓN
 if ($conn->connect_error) {
-    die("Conexión fallida: " . $conn->connect_error);
+    die("Conexión fallida: " . $conn->connect_error)."<br>"; 
 } else {
-    echo "Conexión exitosa";
-    // Aquí puedes realizar consultas y operaciones con la base de datos
-    // Por ejemplo, $conn->query("SELECT * FROM tabla");
+    echo "Conexión exitosa"."<br>";
 }
 
-// Cerrar la conexión cuando hayas terminado
+// INSERCIÓN DE DATOS
+    $sql= "INSERT INTO alumnos (id,nombre,apellidos,curso,ciclo, nota_final) VALUES ('4','Pepa','Martín','SEGUNDO','DAW','7')";
+   if ($conn->query($sql) === TRUE) {
+        echo "Inserción exitosa". "<br>";
+    } else {
+        echo "Error al insertar datos: " . $conn->error."<br>";
+    }
+
+// SELECCIONAR DATOS
+$sql="SELECT * FROM alumnos ";
+$result = $conn->query($sql);  //
+
+if ($result->num_rows > 0) {
+// Trae líneas de la base de datos y muestra los datos
+ while ($row = $result->fetch_assoc()) {
+    echo 
+    " - id: " . $row["id"] . 
+    " - Nombre: " . $row["nombre"] . 
+    " - Apellido: " . $row["apellidos"] . 
+    " - Curso: " . $row["curso"] . 
+    " - Ciclo: " . $row["ciclo"] .
+    " - Nota Final: ". $row["nota_final"]. 
+    "<br>";
+}
+} else {
+echo "No se encontraron resultados";
+}
+
+// ACTUALIZACIÓN DE DATOS
+//   $sql = "UPDATE alumnos SET nombre = 'Antonia' WHERE id = 3";
+//   if ($conn->query($sql) === TRUE) {
+//      echo "Actualización exitosa";
+//    } else {
+//       echo "Error al actualizar datos: " . $conn->error;
+//    }
+
+//ELIMINACIÓN DE DATOS
+   
+    $sql = "DELETE FROM alumnos WHERE nombre = 'Antonio'";
+
+    if ($conn->query($sql) === TRUE) {
+       echo "Eliminación exitosa";
+    } else {
+      echo "Error al eliminar datos: " . $conn->error;
+    }
+
+    
+
+
+
+// CIERRE DE CONEXIÓN
 $conn->close();
 ?>
 
--- EntornoServidor2023/EntornoServidor/PRACTICA_INMOBILIARIA/validadorDatos.php
+++ EntornoServidor2023/EntornoServidor/PRACTICA_INMOBILIARIA/validadorDatos.php
@@ -1,62 +1,321 @@
 <?php
 
+ 
+
+ 
+
+ 
+
 // 1. Función de recogida de datos
+
+ 
+
 function recoge($var, $m = "")
+
+ 
+
 {
+
+ 
+
     $tmp = is_array($m) ? [] : "";
+
+ 
+
     if (isset($_REQUEST[$var])) {
+
+ 
+
         if (!is_array($_REQUEST[$var]) && !is_array($m)) {
+
+ 
+
             $tmp = trim(htmlspecialchars($_REQUEST[$var]));
+
         } elseif (is_array($_REQUEST[$var]) && is_array($m)) {
+
+ 
+
             $tmp = $_REQUEST[$var];
+
+ 
+
             array_walk_recursive($tmp, function (&$valor) {
+
+ 
+
                 $valor = trim(htmlspecialchars($valor));
+
             });
+
         }
+
     }
+
+ 
+
     return $tmp;
+
 }
 
+ 
+
+ 
+
+ 
+
 // 2. Variables que recogen los datos
-$nameV = recoge("tipoVivienda"); 
-$nameZ = recoge("zona");
+
+$tipoVivienda=recoge("tipoVivienda");
+
+$zona=recoge("zona");
+
+$direccion = recoge("direccion");
+
+$dormitorios= recoge("dormitorios");
+
+$precio=recoge("precio");
+
+$tamano=recoge("tamano");
+
+ 
+
+ 
+
+ 
+
+ 
+
+ 
 
 // 3. Variables auxiliares de comprobación
-$nameVOk = false;
-$nameZOk = false;
+
+ 
+
+$direccionOk = false;
+
+$dormitoriosOk=false;
+
+$precioOk=false;
+
+$tamanoOk=false;
+
+ 
 
 // 4. Validación de datos y generación de avisos
 
-    //validación de vivienda y zona
-    if (($nameV =="")||($nameZ =="") ){
-        print "<p>No ha seleccionado opcion\n</p>";
-    }
-    elseif ((is_null($nameV))||(!is_string($nameZ)) ){
-        print "<p>El valor recibido es nulo\n</p>";
-    } 
-    elseif ((ctype_digit($nameV))||(ctype_digit($nameZ))){
-        print "<p>El valor recibido es un dígito\n</p>";
-    }
-    elseif ((ctype_space($nameV))||(ctype_space($nameZ))){
-        print "<p>El valor recibido contiene solo espacios en blanco\n</p>";
-    }
-    elseif ((ctype_punct($nameV))||(ctype_punct($nameZ))){
-        print "<p>El valor recibido contiene caracteres de puntuación\n</p>";
-    }
-
-    else {
-        $nameVOk = true;
-        $nameZOk = true;
-    }
-
-// 5. Ejecución del programa
-if (($nameVOk)&&($nameZOk)) {
-    print "<p>El tipo de vivienda es: . $nameV.</p>\n";
  
-    print "<p>La zona es: . $nameZ.</p>\n";
-   
+
+//validación de dirección
+
+ 
+
+// Define un patrón regular con caracteres permitidos
+
+$patron = '/^[\p{L}\d\sº\/,\-\'#áéíóúüÁÉÍÓÚÜ]+$/u';
+
+// Realiza la validación
+
+if (empty($direccion)) {
+
+    print "La dirección es obligatoria.";
+
 }
 
+elseif (is_numeric($direccion)) {
 
+ 
 
-?>
+    print "<p>No es una dirección válida.</p>";
+
+}
+
+elseif (!preg_match($patron, $direccion)) {
+
+    print "La dirección contiene caracteres no permitidos.";
+
+}
+
+elseif (strlen($direccion) > 200) {
+
+    print "El campo de la dirección excede la longitud máxima permitida (200 caracteres).";
+
+}
+
+else{
+
+    $direccionOk= true;
+
+}
+
+ 
+
+//validación de dormitorios
+
+ 
+
+if ($dormitorios == "") {
+
+    print "<p>No ha seleccionado número de dormitorios.</p>\n";
+
+}
+
+elseif (($dormitorios !== "1" )&& ($dormitorios !== "2") && ($dormitorios !== "3") && ($dormitorios !== "4") && ($dormitorios !== "5" )){
+
+    print "Has introducido una opción no válida";
+
+}
+
+else{
+
+    $dormitoriosOk=true;
+
+}
+
+ 
+
+//validación de precio
+
+ 
+
+if ($precio == "") {
+
+    print "<p>No ha introducido precio</p>\n";
+
+}
+
+elseif (!is_numeric($precio) ) {
+
+    print "Has introducido un valor no numérico";  
+
+}
+
+elseif ($precio<0) {
+
+   print "El precio debe ser mayor que cero";
+
+}
+
+else{
+
+    $precioOk=true;
+
+}
+
+ 
+
+//validación de tamaño
+
+ 
+
+if ($tamano == "") {
+
+    print "<p>No ha introducido tamaño</p>\n";
+
+}
+
+elseif (!is_numeric($tamano) ) {
+
+    print "Has introducido un valor no numérico";  
+
+}
+
+elseif ($tamano<0) {
+
+   print "El tamaño debe ser mayor que cero";
+
+}
+
+else{
+
+    $tamanoOk=true;
+
+}
+
+ 
+
+// Subida de imágenes
+
+ 
+
+if (isset($_POST['subirImagen'])) {
+
+    $nombreImagen = $_FILES['imagen']['name'];
+
+    if (isset($nombreImagen) && $nombreImagen != "") {
+
+        //Obtenemos algunos datos necesarios sobre el archivo
+
+        $tamano = $_FILES['imagen']['size'];
+
+        $temporal = $_FILES['imagen']['tmp_name'];
+
+        //Se comprueba si el archivo a cargar es correcto observando su extensión y tamaño
+
+        $extensionesPermitidas = array("gif", "jpeg", "jpg", "png");
+
+        $extension = pathinfo($nombreImagen, PATHINFO_EXTENSION);
+
+        if (!in_array($extension, $extensionesPermitidas) || $tamano > 2000000) {
+
+            echo '<div><b>Error. La extensión o el tamaño de los archivos no es correcta.<br/>';
+
+        }
+
+        else {
+
+            //Si la imagen es correcta en tamaño y tipo se intenta subir al servidor
+
+            if (move_uploaded_file($temporal, 'images/'.$nombreImagen)){
+
+                echo '<div><b>Se ha subido correctamente la imagen.</b></div>';
+
+                echo '<p><img src="images/'.$nombreImagen.'"></p>';
+
+            }
+
+            else {
+
+                echo '<div><b>Ocurrió algún error al subir el fichero.No pudo guardarse.</b></div>';
+
+            }
+
+        }
+
+    }
+
+}
+
+ 
+
+// 5. Ejecución del programa
+
+ 
+
+print "<p>El tipo de vivienda es:\t $tipoVivienda.</p>\n";
+
+print "<p>La zona es:\t $zona.</p>\n";  
+
+if ($direccionOk) {
+
+    print "<p>La dirección es: $direccion.</p>\n";
+
+}
+
+if ($dormitoriosOk) {
+
+    print "<p>El número de dormitorios es: $dormitorios</p>\n";
+
+}
+
+if ($precioOk) {
+
+    print "<p>El precio es: $precio</p>\n";
+
+}
+
+if ($tamanoOk) {
+
+    print "<p>El precio es: $tamano</p>\n";
+
+}
